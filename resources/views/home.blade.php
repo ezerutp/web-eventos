c7c558c6fb59ea5432cdc3689498d3f4a0340d8e
ezerutp <U22320821@utp.edu.pe>
2025-01-02 23:48:25 -0500

    cambios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,9 +11,9 @@
 <body>
     <header class="header" id="header">
         <nav class="nav container">
-            <a href="" class="nav__log">
+            <a href="" class="nav__logo">
                 <i class="ri-ancient-gate-line"></i>
-                <span>Temple</span>
+                <span>Eventos</span>
             </a>
             <div class="nav__menu" id="nav-menu">
                 <ul class="nav__list">
@@ -41,7 +41,28 @@
     </header>
     <main class="main">
         <section class="home">
-
+            <img src="{{ asset('assets/img/img-bg.svg') }}" alt="img" class="home__bg">
+            <div class="home__container container">
+                <div class="home__data">
+                    <h1 class="home__title">Bienvenido a<br> Eventos</h1>
+                    <p class="home__description">
+                    Organiza tus eventos en un espacio lleno de paz y armonía, 
+                    ideal para bodas, reuniones y celebraciones especiales. 
+                    ¡Haz de tu ocasión un momento inolvidable!
+                    </p>
+                    <a href="#" class="home__button">Reserva Ahora</a>
+                    <img src="{{ asset('assets/img/lantern-1.svg') }}" alt="" class="home__lantern-1">
+                    <img src="{{ asset('assets/img/lantern-2.svg') }}" alt="" class="home__lantern-2">
+                </div>
+                <div class="home__images">
+                    <img src="{{ asset('assets/img/img-1.svg') }}" alt="" class="home__img-1">
+                    <img src="{{ asset('assets/img/img-2.svg') }}" alt="" class="home__img-2">
+                    <img src="{{ asset('assets/img/img-3.svg') }}" alt="" class="home__img-3">
+                    <img src="{{ asset('assets/img/img-4.svg') }}" alt="" class="home__img-4">
+                    <img src="{{ asset('assets/img/img-5.svg') }}" alt="" class="home__img-5">
+                    <img src="{{ asset('assets/img/img-6.svg') }}" alt="" class="home__img-6">
+                </div>
+            </div>
         </section>
     </main>
     @vite('resources/js/main.js')
